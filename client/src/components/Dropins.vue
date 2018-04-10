e1047c06e5966758041e3c1440bdb6fe0a5d5b53
Joe <josephjlawton@gmail.com>
2018-04-10 10:09:01 +0100

    Dropin filtering

--- client/src/components/Dropins.vue
+++ client/src/components/Dropins.vue
@@ -5,8 +5,8 @@
         <h1>All Dropins</h1><br />
         <v-flex xs8 offset-xs2>
           <v-layout row wrap>
-            <v-flex xs2 v-for="day in days" :key="day">
-              <v-checkbox v-bind:label="`${day}`" v-model="selectedDays" v-bind:value="`${day}`"></v-checkbox>
+            <v-flex xs3 v-for="day in days" :key="day">
+              <v-checkbox v-bind:label="`${day}`" v-model="selectedDays" v-bind:value="`${day}`" @change="filter"></v-checkbox>
             </v-flex>
           </v-layout>
           <v-card>
@@ -15,7 +15,7 @@
             </router-link>
           </v-card>
           <v-layout row wrap>
-            <v-flex xs4 v-for="dropin in dropins" :key="dropin.id">
+            <v-flex xs4 v-for="dropin in filteredDropins" :key="dropin.id">
               <v-card>
                 <v-card-title primary-title class="justify-center"><h3 class="headline mb-0">{{dropin.date}}</h3></v-card-title>
                 <v-card-text>{{dropin.attendees.length}} attendees</v-card-text>
@@ -42,12 +42,14 @@ import dateformat from 'dateformat'
 export default {
   data () {
     return {
+      filteredDropins: [],
       dropins: null,
       search: '',
       days: [
-        'Monday',
         'Tuesday',
-        'Wednesday'
+        'Thursday',
+        'Friday',
+        'Sunday'
       ],
       selectedDays: []
     }
@@ -77,14 +79,29 @@ export default {
       var newDate = new Date(date)
       newDate = dateformat(newDate, 'dddd, dS mmmm, yyyy')
       return newDate
+    },
+    filter () {
+      this.filteredDropins = []
+      for (var i = 0; i < this.dropins.length; i++) {
+        for (var j = 0; j < this.selectedDays.length; j++) {
+          const date = new Date(this.dropins[i].rawDate)
+          const day = dateformat(date, 'dddd')
+          if (day === this.selectedDays[j]) {
+            this.filteredDropins.push(this.dropins[i])
+          }
+        }
+      }
     }
   },
   async mounted () {
     const data = (await DropinService.index()).data
     for (var i = 0; i < data.length; i++) {
+      data[i].rawDate = data[i].date
       data[i].date = this.dateParse(data[i].date)
     }
     this.dropins = data
+    this.selectedDays = this.days
+    this.filter()
   }
 }
 
